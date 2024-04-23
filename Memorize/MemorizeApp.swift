c300f1bcd962ba994f5ec7361167efb95759055c
gustavo.kuze <gustavo.silva@ntconsult.com.br>
2024-04-23 14:04:51 -0300

    first commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Gustavo Kuze da Silva on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
